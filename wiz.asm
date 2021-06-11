f3de1277554222c9d956901b4bc5a7e5c1e971b5
Christopher Glenn <topherblair@gmail.com>
2021-06-10 23:53:07 -0700

    Update wiz.asm

--- wiz.asm
+++ wiz.asm
@@ -28,19 +28,21 @@
     SEG.U Variables
     ORG $80
 
-WizXPos         byte                    ; player x-position
-WizYPos         byte                    ; player y-position
-BarbXPos        byte                    ; barbarian x-position
-BarbYPos        byte                    ; barbarian y-position
-TowerHP         byte                    ; tower's hit-points
-Score           byte                    ; player's score
-Level           byte                    ; game level (enemies get faster the higher it goes)
-WizardSpritePtr word                    ; pointer to the P0 sprite lookup table
-BarbSpritePtr   word                    ; pointer to the P1 sprite lookup table
+WizXPos         byte             ; player x-position
+WizYPos         byte             ; player y-position
+BarbXPos        byte             ; barbarian x-position
+BarbYPos        byte             ; barbarian y-position
+WizardHP        byte             ; wizard's hit points
+TowerHP         byte             ; tower's hit-points
+Score           byte             ; player's score
+Level           byte             ; game level (enemies get faster the higher it goes)
+Random          byte             ; randome number generated to set enemy Y
+WizardSpritePtr word             ; pointer to the P0 sprite lookup table
+BarbSpritePtr   word             ; pointer to the P1 sprite lookup table
 
 ; Define Constants
-WIZ_HEIGHT = 9                          ; player0 (wizard) sprite height
-BAR_HEIGHT = 9                          ; player1 (barbarian) sprite height
+WIZ_HEIGHT = 9                   ; player0 (wizard) sprite height
+BAR_HEIGHT = 9                   ; player1 (barbarian) sprite height 
 
 ; *******************************************************************
 ; Start our ROM code at memory address $F000
@@ -52,17 +54,41 @@ BAR_HEIGHT = 9                          ; player1 (barbarian) sprite height
 Reset:
     CLEAN_START                 ; macro to clear memory
 
+; *******************************************************************
+; Initialize RAM variables and TIA registers
+; *******************************************************************
+    LDA #20
+    STA WizXPos                 ; wizard x-position = 20
+    LDA #20
+    STA WizYPos                 ; wizard y-position = 20
+    LDA #2
+    STA WizardHP                ; wizard's hit-points
+    LDA #9
+    STA TowerHP                 ; tower's hit-points
+    lda #%11010100
+    sta Random                  ; Random = $D
+    LDA #0
+    STA Level                   ; starting level
+    STA Score                   ; starting score
+
     LDA #$00                    ; load the hex code for BLACK
     STA COLUBK                  ; set the background to black
 
     LDA #$84                    ; load the hex code for BLUE
-    STA COLUP0                  ; set the player color to blue
+    STA COLUP0                  ; set the player's starting color to blue
 
-; Initialize variables (player position, ect.)
-    LDA #20
-    STA WizXPos                 ; player X position
-    LDA #80
-    STA WizYPos                 ; player Y position
+; *******************************************************************
+; Initialize Pointers to correct lookup table addresses
+; *******************************************************************
+    lda #<WizardSprite
+    sta WizardSpritePtr                 ; lo-byte pointer for wizard table
+    lda #>WizardSprite
+    sta WizardSpritePtr+1               ; hi-byte pointer for wizard table
+
+    lda #<BarbarianSprite
+    sta BarbSpritePtr                   ; lo-byte pointer for wizard table
+    lda #>BarbarianSprite
+    sta BarbSpritePtr+1                 ; hi-byte pointer for wizard table
 
 ; *******************************************************************
 ; START A NEW FRAME
@@ -97,6 +123,8 @@ GameKernal:
     STA WSYNC
     DEX
     BNE GameKernal
+
+
 
 ; OUTPUT 30 LINES OF OVERSCAN
     LDA #2
@@ -214,7 +242,7 @@ Digits:
     .byte %01000100          ; #   #
     .byte %01000100          ; #   #
 
-Wizard:
+WizardSprite:
     .byte #%00000000        ; 
     .byte #%11111001        ; #####  #
     .byte #%01111101        ;  ##### #
@@ -225,7 +253,7 @@ Wizard:
     .byte #%01011001        ;  # ##  #
     .byte #%00110000        ;   ##
 
-Barbarian:
+BarbarianSprite:
     .byte #%00000000        ;
     .byte #%00010010        ;    #  # 
     .byte #%00010010        ;    #  # 
